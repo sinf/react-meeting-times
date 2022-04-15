8319f0395160833f8fcb0a192142a0eef955ea10
ArhoM <arho@amahl.fi>
2022-04-15 18:09:49 +0300

    init_test.sql: implement modify_user_ts() function

--- query/init_test.sql
+++ query/init_test.sql
@@ -34,6 +34,24 @@ CREATE TABLE user_availab (
 
 CREATE INDEX user_availab_idx1 ON user_availab (meeting);
 
+CREATE OR REPLACE FUNCTION
+join_user_ts(i BIGINT, n VARCHAR(28)) RETURNS VOID AS $body$
+
+	UPDATE user_availab SET ts_to = u2.ts_to
+		FROM user_availab u1 INNER JOIN user_availab u2
+		ON (u1.ts_to = u2.ts_from AND u1.status = u2.status)
+		WHERE u1.meeting = i AND u1.username = n AND u2.meeting = i AND u2.username = n;
+
+	DELETE FROM user_availab WHERE id IN (
+		SELECT u2.id
+		FROM user_availab u1 INNER JOIN user_availab u2
+		ON (u1.ts_to = u2.ts_to AND u1.status = u2.status)
+		WHERE u1.meeting = i AND u1.username = n AND u2.meeting = i AND u2.username = n
+			AND u2.ts_from > u1.ts_from);
+
+$body$ LANGUAGE SQL;
+
+
 -- modify_user_ts(meeting id, from, to, username, status)
 CREATE OR REPLACE FUNCTION
 modify_user_ts(
@@ -42,13 +60,37 @@ modify_user_ts(
 	b TIMESTAMP WITH TIME ZONE,
 	n VARCHAR(28),
 	z INTEGER)
-RETURNS VOID AS $$
-	UPDATE user_availab SET ts_to = a WHERE ts_to >= a AND ts_to <= b AND meeting = i AND username = n;
-	UPDATE user_availab SET ts_from = b WHERE ts_from >= a AND ts_from <= b AND meeting = i AND username = n;
-	DELETE FROM user_availab WHERE ts_from >= a AND ts_to <= b AND meeting = i AND username = n;
-	DELETE FROM user_availab WHERE ts_from >= ts_to AND meeting = i AND username = n;
+RETURNS VOID AS $body$
+
+	-- clamp partially overlapping intervals out of the way
+	UPDATE user_availab SET ts_to = a WHERE meeting = i AND username = n AND ts_to >= a AND ts_to <= b;
+	UPDATE user_availab SET ts_from = b WHERE meeting = i AND username = n AND ts_from >= a AND ts_from <= b;
+
+	-- if trying to put a small interval within a big interval: split big interval in 2 and make a gap between them
+	INSERT INTO user_availab (meeting, ts_from, ts_to, username, status)
+		(SELECT meeting, b, ts_to, username, status FROM user_availab
+		WHERE meeting = i AND username = n AND ts_from <= a AND ts_to >= b);
+	UPDATE user_availab SET ts_to = a
+		WHERE meeting = i AND username = n AND ts_from <= a AND ts_to >= b;
+
+	-- delete invalid intervals potentially caused by statements above
+	DELETE FROM user_availab WHERE meeting = i AND username = n AND ts_from >= ts_to;
+
+	-- if inserting big interval over a small one, delete the small interval
+	DELETE FROM user_availab WHERE meeting = i AND username = n AND ts_from >= a AND ts_to <= b;
+
+	-- finally insert the new interval
 	INSERT INTO user_availab (meeting, ts_from, ts_to, username, status) VALUES (i, a, b, n, z);
-$$ LANGUAGE SQL;
+
+	-- try to join adjacent pieces
+	SELECT * FROM "join_user_ts"(i,n); SELECT * FROM "join_user_ts"(i,n);
+	SELECT * FROM "join_user_ts"(i,n); SELECT * FROM "join_user_ts"(i,n);
+	SELECT * FROM "join_user_ts"(i,n); SELECT * FROM "join_user_ts"(i,n);
+	SELECT * FROM "join_user_ts"(i,n); SELECT * FROM "join_user_ts"(i,n);
+	SELECT * FROM "join_user_ts"(i,n); SELECT * FROM "join_user_ts"(i,n);
+	SELECT * FROM "join_user_ts"(i,n); SELECT * FROM "join_user_ts"(i,n);
+
+$body$ LANGUAGE SQL;
 
 INSERT INTO meetings (title, descr, ts_from, ts_to) VALUES ('kokous1', 'blahblahblah', '2018-02-01+0200', '2018-03-01+0200');
 INSERT INTO meetings (title, descr, ts_from, ts_to) VALUES ('kokous2', 'blehBlehbleh', '2018-03-01+0200', '2018-03-15+0200');
@@ -68,4 +110,9 @@ SELECT * FROM "modify_user_ts"(1, '2018-02-01T17:00+0200', '2018-02-01T20:00+020
 
 SELECT * FROM "modify_user_ts"(1, '2018-02-01T15:00+0200', '2018-02-01T22:00+0200', 'testuser', 1);
 -- 14-15, 15-22
+
+SELECT * FROM "modify_user_ts"(1, '2018-02-01T08:00+0200', '2018-02-01T23:00+0200', 'testuser', 1);
+-- 8-23
+
+SELECT * FROM "modify_user_ts"(1, '2018-02-01T14:00+0200', '2018-02-01T14:30+0200', 'testuser', 2);
 
