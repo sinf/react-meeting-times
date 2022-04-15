0af19a69b62c11bf11293ec35ef44da008094280
ArhoM <arho@amahl.fi>
2022-04-15 19:53:18 +0300

    update to react 18, add empty App class

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	pollTimer?: ReturnType<typeof setTimeout>;
+
+	constructor(props: any) {
+		super(props);
+		this.state = {};
+	}
+
+	pollTheServer() {
+		//console.log("polling now");
+	}
+
+	componentDidMount() {
+		this.pollTimer = setInterval(()=>this.pollTheServer(), 1000);
+	}
+
+	componentWillUnmount() {
+		if (this.pollTimer) {
+			clearTimeout(this.pollTimer);
+		}
+	}
+
+	render() {
+  	  return (
+    	 <div className="App">
+    		<br/>
+    		<label htmlFor="username">Username: </label>
+    		<input id="username" name="username" type="text" maxLength={28} />
+    		<button id="username_ok">Enter</button>
+    	 </div>
+  	  );
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom'; // react 17
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = document.getElementById('root')!;
+const my_react = (
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
+
+// ReactDOM.render(my_react, root); // react 17 way
+ReactDOM.createRoot(root).render(my_react);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
